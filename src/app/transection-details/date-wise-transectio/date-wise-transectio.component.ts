20f5c54de25b397ccf7949aee240006ae618f88c
FPS\arif.tamboli <arif.tamboli@nihilent.com>
2018-11-15 17:54:44 +0530

    #2 feat: done with some html code and also rename  files from css to scss

--- src/app/transection-details/date-wise-transectio/date-wise-transectio.component.ts
+++ src/app/transection-details/date-wise-transectio/date-wise-transectio.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-date-wise-transectio',
   templateUrl: './date-wise-transectio.component.html',
-  styleUrls: ['./date-wise-transectio.component.css']
+  styleUrls: ['./date-wise-transectio.component.scss']
 })
 export class DateWiseTransectioComponent implements OnInit {
 
