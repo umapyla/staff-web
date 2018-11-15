963a537ac6cd21df39d89218104ad29a2fdc1df0
FPS\amol.lakde <amol.lakde@nihilent.com>
2018-11-15 11:16:20 +0530

    fix: fix review comments

---
+++ src/app/Constant.ts
@@ -0,0 +1,9 @@
+export class Constant {
+  public static Messages =  {
+    activateMsgBody: 'you will be able to pay online with your card.',
+    activateMsgHeader: 'Activate online Service.?',
+    deactivateMsgHeader: 'Deactivate online purchases.?',
+    deactivateMsgBody: 'you wont be able to pay online with your card.',
+    flag: false
+  };
+}
