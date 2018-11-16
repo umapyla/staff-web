b565b843ed6d573aa8b32f0882902e90f76d86d2
kajal.chavan <kajal.chavan@nihilent.com>
2018-11-16 11:01:39 +0530

    newly added code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { DemoComponent } from './demo/demo.component';
+import { NgbModule   } from '@ng-bootstrap/ng-bootstrap';
 
+// import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+// import { ToastModule } from 'ng2-toastr';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DemoComponent
   ],
   imports: [
-    BrowserModule
+    FormsModule,
+    BrowserModule,
+    // BrowserAnimationsModule,
+    NgbModule.forRoot()
+    //  ToastModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
