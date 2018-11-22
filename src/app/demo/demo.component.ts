25c25e32c870b180b4398c67dc4faf74acba17b2
kajal.chavan <kajal.chavan@nihilent.com>
2018-11-22 11:46:54 +0530

    Fixed latest issues

--- src/app/demo/demo.component.ts
+++ src/app/demo/demo.component.ts
@@ -1,36 +1,43 @@
-import { Component, OnInit, ViewContainerRef } from '@angular/core';
+import { Component, OnInit, ViewContainerRef } from "@angular/core";
 import {
   NgbModal,
   ModalDismissReasons,
   NgbModalRef
-} from '@ng-bootstrap/ng-bootstrap';
-import { constants } from '../constants';
+} from "@ng-bootstrap/ng-bootstrap";
+import { constants } from "../constants";
 
 @Component({
-  selector: 'app-demo',
-  templateUrl: './demo.component.html',
-  styleUrls: ['./demo.component.scss']
+  selector: "app-demo",
+  templateUrl: "./demo.component.html",
+  styleUrls: ["./demo.component.scss"]
 })
 export class DemoComponent implements OnInit {
   public isCardFreeze: boolean;
   clicked = true;
   private modalRef: NgbModalRef;
-  openModal = true;
   labels = constants.labels.freeze;
+  public isNewFreeze: boolean;
   displayLabel = false;
+  isOpen: boolean;
+  openModal: true;
   constructor(private modalService: NgbModal) {}
   ngOnInit() {}
 
-  open(content) {
-    this.displayLabel = true;
+  openNew() {
+    this.labels.freezeConfirm = this.openModal ? this.labels.freezeConfirm : this.labels.unfreeze;
+    this.labels.freezeNote = this.openModal ? this.labels.freezeNote  : this.labels.transactionEnabled;
+    this.labels.lostStolen = this.openModal ? this.labels.lostStolen : '';
+    this.labels.freezeCard = this.openModal && !this.isCardFreeze ? this.labels.freezeCard : this.labels.unfreezeCard;
+  }
+  open(content, value: string) {
+    this.displayLabel = false;
     this.modalRef = this.modalService.open(content);
   }
-
   openLg(content) {
     this.modalService.open(content);
   }
-
   onNextClick(value: string) {
+    this.displayLabel = true;
     this.isCardFreeze = value === this.labels.freeze ? true : false;
   }
 }
