0f7f42dab82ace900670fa1911a4173b1a3ee6c1
FPS\kshitija.bidwai <kshitija.bidwai@nihilent.com>
2018-11-28 12:55:49 +0530

    #1 feat: Display search data according search

--- src/app/transaction-search.service.spec.ts
+++ src/app/transaction-search.service.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed, inject } from '@angular/core/testing';
 
-import { Observable } from 'rxjs/Observable';
+import { Observable } from 'rxjs';
 import { TransactionSearchService } from './transaction-search.service';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { HttpClient } from '@angular/common/http';
